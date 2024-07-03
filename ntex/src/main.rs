2830641ccbfcb6845d0f341a5c9f9f2052cde30e
HallZero <filipikikuchi@gmail.com>
2024-07-03 11:50:30 -0300

    feat(state): :sparkles: Add AppState to App

--- ntex/src/main.rs
+++ ntex/src/main.rs
@@ -1,5 +1,9 @@
 use ntex::web;
 
+struct AppState {
+    app_name: String,
+}
+
 #[web::get("/")]
 async fn hello() -> impl web::Responder {
     web::HttpResponse::Ok().body("Hello World!")
@@ -10,6 +14,12 @@ async fn echo(req_body: String) -> impl web::Responder{
     web::HttpResponse::Ok().body(req_body)
 }
 
+#[web::get("/test")]
+async fn test(data: web::types::State<AppState>) -> impl web::Responder {
+    let app_name = &data.app_name;
+    format!("Hello {app_name}")
+}
+
 async fn manual_hello() -> impl web::Responder {
     web::HttpResponse::Ok().body("Hey There!")
 }
@@ -18,9 +28,16 @@ async fn manual_hello() -> impl web::Responder {
 async fn main() -> std::io::Result<()>{
     web::HttpServer::new(|| {
         web::App::new()
-        .service(hello)
-        .service(echo)
-        .route("/hey", web::get().to(manual_hello))
+            .state(AppState {
+                app_name: String::from("Ntex")
+            })
+            .service(
+                web::scope("/testing")
+                    .service(test)   
+            )
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
     })
     .bind(("0.0.0.0", 8080))?
     .run()
