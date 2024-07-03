352cf0f237481bb520cb7ddb9e4f4a8c2c268864
HallZero <filipikikuchi@gmail.com>
2024-07-03 14:08:48 -0300

    feat(extractors): :sparkles: Add extractors

--- README.md
+++ README.md
@@ -12,6 +12,12 @@ curl --proto 'https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 
 ## Reference material
 
+### Basics
+
 [The Rust Programming Language](https://doc.rust-lang.org/book/) -> This book gives some good explanations of how to get Started with Rust
 
 [Let's get Rusty](https://www.youtube.com/playlist?list=PLai5B987bZ9CoVR-QEIN9foz4QCJ0H2Y8) -> Playlist containing topics from basic to advanced in Rust
+
+### Frameworks
+
+[Ntex Documentation](https://ntex.rs/) -> Docs for Ntex framework
--- ntex/src/main.rs
+++ ntex/src/main.rs
@@ -1,7 +1,13 @@
 use ntex::web;
+use serde::Deserialize;
 
 struct AppState {
     app_name: String,
+}
+
+#[derive(Deserialize)]
+struct Info {
+    username: String,
 }
 
 #[web::get("/")]
@@ -24,6 +30,32 @@ async fn manual_hello() -> impl web::Responder {
     web::HttpResponse::Ok().body("Hey There!")
 }
 
+
+// Extractors
+
+
+#[web::get("/users/{user_id}/{friend}")]
+async fn path_extractor(
+    path: web::types::Path<(String, String)>
+) -> Result<String, web::Error> {
+    let path = path.into_inner();
+    Ok(format!("{} {}", path.0, path.1))
+}
+
+#[web::get("/query")]
+async fn query_extractor(info: web::types::Query<Info>) -> String {
+    format!("Welcome {}", info.username)
+}
+
+#[web::post("/json")]
+async fn json_extractor(info: web::types::Json<Info>) -> Result<String, web::Error> {
+    Ok(format!("Welcome {}!", info.username))
+}
+
+
+// Main
+
+
 #[ntex::main]
 async fn main() -> std::io::Result<()>{
     web::HttpServer::new(|| {
@@ -37,6 +69,9 @@ async fn main() -> std::io::Result<()>{
             )
             .service(hello)
             .service(echo)
+            .service(path_extractor)
+            .service(query_extractor)
+            .service(json_extractor)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("0.0.0.0", 8080))?
